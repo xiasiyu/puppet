4bf8297ece8930d74e30c06eb10f51dc54896296
gl <gl@guanglei.local>
2014-08-27 18:21:00 +0800

    back compitable for puppet 3.0.1

--- modules/common/verify_puppet_installation/manifests/init.pp
+++ modules/common/verify_puppet_installation/manifests/init.pp
@@ -1,3 +1,11 @@
 class verify_puppet_installation {
-  include createFileForNow
+#  include createFileForNow
+Exec {
+path => ['/usr/bin','/usr/sbin','/bin', '/usr/local/bin', '/usr/local/share/bin']
+}
+
+exec { 'create file':
+  cwd    => '/tmp',
+  command  => "date > now.txt";
+}
 }
