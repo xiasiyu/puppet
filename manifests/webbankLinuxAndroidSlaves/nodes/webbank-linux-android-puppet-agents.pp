76247f03ce7804e6ac11bcc4ce3fe75fe098d657
gl <gl@guanglei.local>
2014-08-27 13:53:33 +0800

    add common support

--- manifests/webbankLinuxAndroidSlaves/nodes/webbank-linux-android-puppet-agents.pp
+++ manifests/webbankLinuxAndroidSlaves/nodes/webbank-linux-android-puppet-agents.pp
@@ -1,4 +1,5 @@
 node /webank-linux-android-puppet-agent-.*.wsd.com/ {
+  include verify_puppet_installation
   include webbank_linux_android_slave
 }
 
---
+++ modules/common/verify_puppet_installation/manifests/init.pp
@@ -0,0 +1,10 @@
+class verifyPuppetInstallation {
+  Exec {
+    path => ['/usr/bin','/usr/sbin','/bin', '/usr/local/bin', '/usr/local/share/bin']
+  }
+
+  exec { 'create file':
+    cwd    => '/tmp',
+    command  => "date > now.txt";
+  }
+}
